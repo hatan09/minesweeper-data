44939dfb0cdd29dafa4ea6260c6ce0fc964e7c8a
Xerath <abcxyz01233210@gmail.com>
2022-04-21 02:15:59 +0700

    finalize

--- mongodb.js
+++ mongodb.js
@@ -6,35 +6,78 @@ const MongoObject = {
     resultArray : 0,
     
     getAllUsers : function(callback){
-        MongoClient.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}, (err, db) => {
-            if (err) throw err;
-            var dbo = db.db("example0");
-            dbo.collection("users").find({}).toArray((err, resultArray) => {
-              if(err) throw err;
-              callback(resultArray);
-            })
-          });
+      MongoClient.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}, (err, db) => {
+          if (err) throw err;
+          var dbo = db.db("minesweeperdata");
+          dbo.collection("users").find({}).toArray((err, resultArray) => {
+            if(err) throw err;
+            db.close();
+            callback(resultArray);
+          })
+        });
     },
 
-    findUserById : function(id, callback){
-
+    findById : function(id, callback){
+      MongoClient.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}, (err, db) => {
+        if (err) throw err;
+        var dbo = db.db("minesweeperdata");
+        var query = { id: id };
+        dbo.collection("users").find(query).toArray((err, resultArray) => {
+          if(err) throw err;
+          db.close();
+          callback(resultArray);
+        })
+      });
     },
 
-    addUser : function(data){
+    findByUsername : function(username, callback){
+      MongoClient.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}, (err, db) => {
+        if (err) throw err;
+        var dbo = db.db("minesweeperdata");
+        var query = { username: username };
+        dbo.collection("users").find(query).toArray((err, resultArray) => {
+          if(err) throw err;
+          db.close();
+          callback(resultArray);
+        })
+      });
+    },
+
+    addUser : async function(data, callback){
         MongoClient.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}, (err, db) => {
             if (err) throw err;
-            var dbo = db.db("example0");
+            var dbo = db.db("minesweeperdata");
             var myobj = data;
+            var user = this.findByUsername(data.username);
+            console.log(user);
             dbo.collection("users").insertOne(myobj, (err, res) => {
               if (err) throw err;
               console.log("1 document inserted");
+            });
+            dbo.collection("users").find({id: data.id}).toArray((err, resultArray) => {
+              if(err) throw err;
               db.close();
+              callback(resultArray);
             });
         });
     },
 
-    updateUser : function(data){
-
+    updateUser : function(id, obj, callback){
+      MongoClient.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}, (err, db) => {
+        if (err) throw err;
+        let query = {id : id};
+        let newValues = { $set: obj};
+        var dbo = db.db("minesweeperdata");
+        dbo.collection("users").updateOne(query, newValues, function(err, res) {
+          if (err) throw err;
+          console.log("1 document updated");
+        });
+        dbo.collection("users").find({id: id}).toArray((err, resultArray) => {
+          if(err) throw err;
+          db.close();
+          callback(resultArray);
+        });
+    });
     },
 
     deleteUser : function(data){
--- routes/api.js
+++ routes/api.js
@@ -3,59 +3,65 @@ const router = express.Router();
 const MongoObject = require('../mongodb');
 const uuid = require('uuid');
 
-//get all users
+// get all users
 router.get('/getall', (req, res) => {
     MongoObject.getAllUsers((data) => {
         res.json(data);
     })
 });
 
-//get user with id
+// get user with id
+// url/api/getbyid/123
 router.get('/getbyid/:id', (req, res) => {
-    res.send(req.params.id); 
-
-    MongoObject.getAllUsers((data) => {
+    MongoObject.findById(req.params.id, (data) =>{
         res.json(data);
-    })
+    });
+});
+
+// login
+router.get('/login/:username', (req, res) => {
+    let username = req.params.username;
+
+    MongoObject.findByUsername(username, (data) =>{
+        res.json(data);
+    });
 });
 
 //create user
 router.post('/create', (req, res) =>{
-    console.log(req.body);
+    //console.log(req.body);
     const newUser = {
         id: uuid.v4(),
         name: req.body.name,
         username: req.body.username,
-        pass: req.body.pass,
         highest_score: 0,
     }
 
-    if(!newUser.name || !newUser.username || !newUser.pass){
+    if(!newUser.name || !newUser.username){
         res.status(400).json({msg: 'Please provide adequate info!'});
         return;
     }
 
-    MongoObject.addUser(newUser);
-    res.json({msg: 'OK'})
+    MongoObject.addUser(newUser, (data) => {
+        res.json(data);
+    });
+    
 });
 
 // update user
-router.post('/update', (req, res) =>{
-    const newUser = {
-        id: uuid.v4(),
-        name: req.body.name,
-        username: req.body.username,
-        pass: req.body.pass,
-        highest_score: 0,
-    }
+router.post('/update/:id/:field&:value', (req, res) =>{
+    let id = req.params.id;
+    let fields = req.params.field.split(",");
+    let values = req.params.value.split(",");
+    let obj = {}
 
-    if(!newUser.name || !newUser.username || !newUser.pass){
-        res.status(400).json({msg: 'Please provide adequate info!'});
-        return;
-    }
+    fields.map((data, i) => {
+        obj[data] = values[i];
+    })
 
-    MongoObject.addAUser(newUser);
-    res.json({msg: newUser.id});
+    MongoObject.updateUser(id, obj, (data) =>{
+        res.json(data);
+    });
 });
 
 // delete user
